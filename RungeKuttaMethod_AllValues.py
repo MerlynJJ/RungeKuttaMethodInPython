c7043d72975c7f54c782b9bf2a9f7239fad90932
Merlyn JJG <merlynjjg@gmail.com>
2023-06-19 19:40:30 -0600

    rename the file and point out that are different

--- RungeKuttaMethod_AllValues.py
+++ RungeKuttaMethod_AllValues.py
@@ -6,8 +6,10 @@ Created on Tue May 17 14:13:12 2022
 @author: merlyn
 """
 
-# first order differential equations
-# Second order differential equations
+#these two integrator functions throw one value at a time
+
+# first order differential equations INTEGRATOR
+
 def RKN4_1fstODE(f,y,t,h,params):
     # slope at the beginning of the time step
     k1 = f(y,t,params)
@@ -17,18 +19,19 @@ def RKN4_1fstODE(f,y,t,h,params):
     k2 = f(y1,t+(h/2),params)
     # use the slope k2 to step halfway through the time step
     y2 = y + k2*h/2
-    # another estimate of the slope at the midpoint
+    # another estimate of the slope at the y2 midpoint
     k3 = f(y2,t+(h/2),params)
     # use the slope, k3, to step all the way across the time step (to t₀+h)
     y3 = y + k3*(h/2)
     # an estimate of the slope at the endpoint
     k4 = f(y3, t + h,params)
     # use a weighted sum of these slopes to get our final estimate of y*(t₀+h)
-    V = y + (h/6)*(k1+2*k2+2*k3+k4)
-    return V
+    Y = y + (h/6)*(k1+2*k2+2*k3+k4)
+    return Y
 
 
-# Second order differential equations
+# Second order differential equations INTEGRATOR
+
 def RKN4_2ndODE(f,y,v,t,h,params):
     # slope at the beginning of the time step
     k1 = f(y,v,t,params)
@@ -50,5 +53,5 @@ def RKN4_2ndODE(f,y,v,t,h,params):
     # use a weighted sum of these slopes to get our final estimate of y*(t₀+h)
     V = v + (h/6)*(k1+2*k2+2*k3+k4)
     Y = y + (h/6)*(v1+2*v2+2*v3+V)
-    return k4,Y,V
+    return Y,V
 
